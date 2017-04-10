aaf1deabeeeb95ae7182eb14e01154148e715e98
Kular <kular.ai@gmail.com>
2017-04-10 17:45:12 +0900

    update auto-build to archvie manifest

--- auto-build.sh
+++ auto-build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 usage_exit() {
-  echo "Usage: sh $0 [-v VERSION] [-d DATE] [-n BINARY_NAME] [-p BINARY_PATH]"
+  echo "Usage: sh $0 [-v VERSION] [-b PREV_DATE] [-d DATE] [-n BINARY_NAME] [-p BINARY_PATH]"
   exit 1
 }
 
-while getopts v:d:n:p:h OPT
+while getopts v:b:d:n:p:h OPT
 do
   case $OPT in
     v) VERSION=$OPTARG;;
+    b) PREV_DATE=$OPTARG;;
     d) DATE=$OPTARG;;
     n) BINARY_NAME=$OPTARG;;
     p) BINARY_PATH=$OPTARG;;
@@ -17,6 +18,12 @@ do
   esac
 done
 
+cp ./manifest.plist ./Archives/manifest_$PREV_DATE.plist
+echo "[Done] Archive manifest_"$PREV_DATE
+
+gsed -i "s/$PREV_DATE/$DATE/g" ./manifest.plist
+echo "[Done] Update manifest"
+
 ACCESS_TOKEN=`cat ./secret/access_token`
 echo $ACCESS_TOKEN
 
@@ -24,6 +31,7 @@ TAG=$VERSION-$DATE
 git tag $TAG && git push --tags
 echo "[Done] push tag:" $TAG
 
+echo "Now Releasing:" $TAG
 sh ./app_release.sh \
     -t $ACCESS_TOKEN \
     -v $VERSION \
